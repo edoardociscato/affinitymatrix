0efd0fb3cd550cf81c354522221a92841557110e
edoardociscato <edoardo.ciscato@kuleuven.be>
2022-06-11 19:34:33 +0200

    New ggrepel functions

--- R/Descriptive.R
+++ R/Descriptive.R
@@ -504,8 +504,9 @@ show.correlations = function(res,
       ggrepel::geom_text_repel(data = cor[cor$filter,],
                                ggplot2::aes_string(x="x",y="y",label="label",
                                                    hjust="hjust",vjust="vjust",
-                                                   fontface="font"),
-                               size=2, segment.size=0.25, seed=1) +
+                                                   fontface="font",xlim=),
+                               size=2, segment.size=0.25, seed=1, max.overlaps=Inf,
+                               xlim=c(NA,NA), ylim=c(NA,NA)) +
       ggplot2::labs(x=paste0(label_x_axis,", dim. ",d),
                     y=paste0(label_y_axis,", dim. ",d))
 
